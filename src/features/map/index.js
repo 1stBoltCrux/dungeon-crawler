0f4197c931047743f13eb12b5eb9682ae91b303e
tedLeary <markeleary@gmail.com>
2018-11-02 13:33:23 -0700

    set up movement functions and basic structure

---
+++ src/features/map/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Map() {
+  return(
+    <div
+      style={{
+        width: '800px',
+        height: '400px',
+        backgroundColor: 'black',
+        border: '4px, solid, white',
+      }}/>
+  )
+}
+
+export default Map
---
+++ src/features/player/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import skele from '../../assets/images/skele.png'
+import handleMovement from './movement'
+
+function Player(props) {
+  return (
+    <div style={{
+      position: 'absolute',
+      top: props.position[1],
+      left: props.position[0],
+      backgroundImage: `url('${skele}')`,
+      backgroundPosition: '0  384px',
+      width: '70px',
+      height: '70px'
+    }}>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    ...state.player
+  }
+}
+
+export default connect(mapStateToProps)(handleMovement(Player))
