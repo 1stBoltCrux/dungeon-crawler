b84bac1b588051ebb59d7f0c362ed4fce86f2b65
tedLeary <markeleary@gmail.com>
2018-11-02 17:34:12 -0700

    add impassable objects logic

--- src/features/map/index.js
+++ src/features/map/index.js
@@ -1,15 +1,61 @@
 import React from 'react'
+import {SPRITE_SIZE} from '../../config/constants'
+import './styles.css'
+import { connect } from 'react-redux'
 
-function Map() {
+function getTileSprite(type){
+  switch(type) {
+    case 0:
+      return 'grass'
+    case 4:
+      return 'stone-floor'
+    case 5:
+      return 'rock'
+    case 6:
+      return 'tree'
+
+  }
+}
+
+function MapTile(props){
+  return <div
+    style={{
+      height: SPRITE_SIZE ,
+      width:  SPRITE_SIZE
+    }}
+   className={`tile ${getTileSprite(props.tile)}`}>
+
+   </div>
+}
+
+function MapRow(props){
+  return <div className="row">
+  {
+    props.tiles.map( tile => <MapTile tile={tile}/>)
+  }
+  </div>
+}
+
+function Map(props) {
   return(
     <div
       style={{
-        width: '1200px',
-        height: '600px',
+        width: SPRITE_SIZE * 20,
+        height: SPRITE_SIZE * 10,
         backgroundColor: 'black',
         border: '4px, solid, white',
-      }}/>
+      }}>
+      {
+        props.tiles.map(row =>  <MapRow tiles={row}/>)
+      }
+      </div>
   )
 }
 
-export default Map
+function mapStateToProps(state) {
+  return {
+    tiles: state.map.tiles
+  }
+}
+
+export default connect(mapStateToProps)(Map)
--- src/features/world/index.js
+++ src/features/world/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Map from '../map'
 import Player from '../player'
+import { tiles } from '../../config/data/maps/1'
+import store from '../../config/store'
 
 function World(props) {
+  store.dispatch({
+    type: 'ADD_TILES',
+    payload: {
+      tiles: tiles
+    }
+  })
   return(
     <div
     style={{
       position: 'relative',
       width: '1200px',
       height: '600px',
-      margin: '20px auto'
+      margin: 'auto'
     }}
     >
     <Map/>
